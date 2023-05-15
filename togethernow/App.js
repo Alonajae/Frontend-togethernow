8783ce6e60b2a5eab8ee24def0b309e224c6436c
Thomas <thomas.pierreleandre972@gmail.com>
2023-05-15 15:20:26 +0200

    t

--- togethernow/App.js
+++ togethernow/App.js
@@ -1,5 +1,39 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './Screens/HomeScreen';
+import IdentityScreen from './Screens/IdentityScreen';
+import MapScreen from './Screens/MapScreen';
+import TakePictureScreen from './Screens/TakePictureScreen';
+import { Provider } from 'react-redux';
+import { PersistConfig } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import user from './reducers/user';
+
+const persistConfig = {
+  key: 'TogetherNow',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, user);
+
+const reducers = combineReducers({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }),
+});
+
+const store = configureStore({
+  reducer: reducers,
+});
+
+const persistor = persistStore(store);
+
+const Stack = createNativeStackNavigator();
+
+
+
 
 export default function App() {
   return (
