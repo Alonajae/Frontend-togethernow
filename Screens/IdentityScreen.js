e2386a672f6563575c75a3086b77761c1840fc2a
camillepic <camillejpicard@gmail.com>
2023-05-16 10:50:17 +0200

    update identity screen

--- Screens/IdentityScreen.js
+++ Screens/IdentityScreen.js
@@ -1,58 +1,3 @@
-// import { View, Text } from 'react-native'
-// import React from 'react'
-// import { Camera, CameraType } from 'expo-camera'
-// import { useState, useEffect, useRef } from 'react'
-// import { TouchableOpacity } from 'react-native'
-// import { Ionicons } from '@expo/vector-icons'
-// import { useDispatch } from 'react-redux'
-// import { useIsFocused } from '@react-navigation/native'
-
-// export default function IdentityScreen ({ navigation }) {
-
-//   // camera states
-//   const [hasPermission, setHasPermission] = useState(null)
-//   const [type, setType] = useState(CameraType.back);
-//   const [flash, setFlash] = useState(Camera.Constants.FlashMode.off)
-//   const [camera, setCamera] = useState(null)
-//   const isFocused = useIsFocused();
-//   const dispatch = useDispatch();
-
-//   let cameraRef = useRef(null);
-
-//   // ask for permission
-//   useEffect(() => {
-//     (async () => {
-//       const { status } = await Camera.requestCameraPermissionsAsync();
-//       setHasPermission(status === 'granted');
-//     })()
-//   }, [])
-
-//   // take the picture
-//   const takePicture = async () => {
-//     const photo = await cameraRef.takePictureAsync({ quality: 0.3 });
-//     console.log(photo.width, photo.height);
-//     console.log(photo.uri);
-//     // store the picture in redux
-//   }
-
-//   // if no permission, return empty view
-//   if (!hasPermission || !isFocused) {
-//     return <View></View>;
-//   }
-
-
-//   return (
-//     <Camera type={type} ref={(ref) => cameraRef = ref}>
-//       <Button
-//         title="Flip"
-//         onPress={() => setType(type === CameraType.back ? CameraType.front : CameraType.back)}
-//       />
-//       <Button title="Snap" onPress={() => takePicture()} />
-//     </Camera>
-//   )
-// }
-
-
 import React, { useState, useEffect, useRef } from "react";
 import { Button, StyleSheet, Text, View } from "react-native";
 import Voice from "@react-native-voice/voice";
@@ -163,3 +108,58 @@ const styles = StyleSheet.create({
     marginBottom: 8,
   },
 });
+
+
+// import { View, Text } from 'react-native'
+// import React from 'react'
+// import { Camera, CameraType } from 'expo-camera'
+// import { useState, useEffect, useRef } from 'react'
+// import { TouchableOpacity } from 'react-native'
+// import { Ionicons } from '@expo/vector-icons'
+// import { useDispatch } from 'react-redux'
+// import { useIsFocused } from '@react-navigation/native'
+
+// export default function IdentityScreen ({ navigation }) {
+
+//   // camera states
+//   const [hasPermission, setHasPermission] = useState(null)
+//   const [type, setType] = useState(CameraType.back);
+//   const [flash, setFlash] = useState(Camera.Constants.FlashMode.off)
+//   const [camera, setCamera] = useState(null)
+//   const isFocused = useIsFocused();
+//   const dispatch = useDispatch();
+
+//   let cameraRef = useRef(null);
+
+//   // ask for permission
+//   useEffect(() => {
+//     (async () => {
+//       const { status } = await Camera.requestCameraPermissionsAsync();
+//       setHasPermission(status === 'granted');
+//     })()
+//   }, [])
+
+//   // take the picture
+//   const takePicture = async () => {
+//     const photo = await cameraRef.takePictureAsync({ quality: 0.3 });
+//     console.log(photo.width, photo.height);
+//     console.log(photo.uri);
+//     // store the picture in redux
+//   }
+
+//   // if no permission, return empty view
+//   if (!hasPermission || !isFocused) {
+//     return <View></View>;
+//   }
+
+
+//   return (
+//     <Camera type={type} ref={(ref) => cameraRef = ref}>
+//       <Button
+//         title="Flip"
+//         onPress={() => setType(type === CameraType.back ? CameraType.front : CameraType.back)}
+//       />
+//       <Button title="Snap" onPress={() => takePicture()} />
+//     </Camera>
+//   )
+// }
