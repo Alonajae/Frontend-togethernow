b6ddd4b8383105d493cad9d568d99e55e7aa076a
frederiquembi <frederiquembime@gmail.com>
2023-05-17 19:41:11 +0200

    maj login

--- Test/MapScreen Fred.js
+++ Test/MapScreen Fred.js
@@ -1,57 +1,45 @@
 import React, { useEffect, useState } from 'react';
+import MapView, { Marker } from 'react-native-maps';
+import * as Location from 'expo-location';
 
-const App = () => {
-  const [safePlaces, setSafePlaces] = useState([]);
-  const [buddies, setBuddies] = useState([]);
-  const [alerts, setAlerts] = useState([]);
+export default function MapScreen() {
+
+    const [safePlaces, setSafePlaces] = useState([]);
+    const [buddies, setBuddies] = useState([]);
+    const [alerts, setAlerts] = useState([]);
 
   useEffect(() => {
-    const fetchSafePlaces = async (latitude, longitude) => {
-      try {
-        const response = await fetch(
-          `https://BACKEND/api/safeplaces?${coordonate}`
-        );
-        const data = await response.json();
-        setSafePlaces(data); // mise à jour de l'état 
-      } catch (error) {
-        console.error(error);
-      }
-    };
+    (async () => {
+      const { status } = await Location.requestForegroundPermissionsAsync();
 
-    const fetchBuddies = async (latitude, longitude) => {
-      try {
-        const response = await fetch(
-          `https://BACKEND/api/buddies?${coordonate}`
-        );
-        const data = await response.json();
-        setBuddies(data);
-      } catch (error) {
-        console.error(error);
+      if (status === 'granted') {
+        Location.watchPositionAsync({ distanceInterval: 10 },
+          (location) => {
+            console.log(location);
+            setCurrentPosition(location);
+          });
       }
-    };
+    })();
+  })
 
-    const fetchAlerts = async (latitude, longitude) => {
-      try {
-        const response = await fetch(`https://BACKEND/api/alerts?${coordonate}`
-        );
-        const data = await response.json();
-        setAlerts(data); 
-      } catch (error) {
-        console.error(error);
-      }
-    };
+  useEffect(() => {
+    (async () => {
+        const result = await fetch('http://URL:3000/safeplaces');
+        const data = await result.json();
+        console.log(data);
+        setSafePlaces(data.safeplaces);
+    })();
+    }, []);
 
-    const getUserLocation = () => {
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(
-          (position) => {
-            const { latitude, longitude } = position.coords;
-            fetchSafePlaces(latitude, longitude);
-            fetchBuddies(latitude, longitude);
-            fetchAlerts(latitude, longitude);
-          },
-          (error) => {
-            console.error(error);
-          }
-        );
-      }  };)}})();}, []);
+    const buddies fetch('http://URL:3000/buddies');
+    const data = await result.json();               
+    setBuddies(data.buddies);
+    })();
+    }, []);
+
+    const alerts fetch('http://URL:3000/alerts');
+    const data = await result.json();
+    setAlerts(data.alerts);
+    })();   
+    }, []);
+    }
