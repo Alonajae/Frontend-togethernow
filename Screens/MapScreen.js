21d17d817a3b357b5d4fb0f2742da4e60969c12a
Thomas <thomas.pierreleandre972@gmail.com>
2023-05-21 11:12:33 +0200

    réglages sur la map pour son bon affichage des buddies

--- Screens/MapScreen.js
+++ Screens/MapScreen.js
@@ -41,19 +41,20 @@ export default function MapScreen({ navigation }) {
     }
   });
 
-  
+
   // create markers for buddies, safe places and alerts
 
   const buddiesMarkers = buddies.map((buddy, i) => {
+    if (buddy.currentLocation.latitude === 0 && buddy.currentLocation.longitude === 0) return null;
     return (
       <Marker
         key={i}
-        coordinate={buddy.coordinate}
+        coordinate={buddy.currentLocation}
         title={buddy.firstname}
         description={buddy.firstname}
-        >
-          <Image source={require('../assets/icons8-location-48.png')} />
-        </Marker>
+      >
+        <Image source={require('../assets/icons8-location-48.png')} />
+      </Marker>
     );
   });
 
@@ -106,7 +107,7 @@ export default function MapScreen({ navigation }) {
             })
               .then((response) => response.json())
               .then((data) => {
-                console.log(data);
+                console.log(location);
               }
               )
           });
@@ -135,13 +136,14 @@ export default function MapScreen({ navigation }) {
 
   // handle the long press on the map to create an alert and open the modal
 
-  const handleLongPress = (e) => {
+  const handleLongPress = (infos) => {
+    console.log(infos.nativeEvent.coordinate);
     setAlertModalVisible(true);
     setNewAlertInfos({
       ...newAlertInfos,
       coordinate: {
-        latitude: e.nativeEvent.coordinate.latitude,
-        longitude: e.nativeEvent.coordinate.longitude
+        latitude: infos.nativeEvent.coordinate.latitude,
+        longitude: infos.nativeEvent.coordinate.longitude
       }
     })
   }
@@ -190,7 +192,7 @@ export default function MapScreen({ navigation }) {
 
 
   // handle the position of the user
-  let currentPos = null;
+  let currentPos;
   if (currentPosition) {
     currentPos = (
       <Marker
@@ -199,6 +201,8 @@ export default function MapScreen({ navigation }) {
         description="Your current position"
       />
     )
+  } else {
+    currentPos = null;
   }
 
   // make the initial region of the map the current position of the user
@@ -223,32 +227,32 @@ export default function MapScreen({ navigation }) {
 
   // travail de Fred
 
-  let googleBar = (
-    <GooglePlacesAutocomplete
-    placeholder='Search'
-    fetchDetails={true}
-    onPress={(data, details = null) => {
-      console.log(data, details);
-    }}
-    query={{
-      key: GOOGLE_PLACES_API_KEY,
-      language: 'en',
-    }}
-  />
-  )
+  // let googleBar = (
+  //   <GooglePlacesAutocomplete
+  //   placeholder='Search'
+  //   fetchDetails={true}
+  //   onPress={(data, details = null) => {
+  //     console.log(data, details);
+  //   }}
+  //   query={{
+  //     key: GOOGLE_PLACES_API_KEY,
+  //     language: 'en',
+  //   }}
+  // />
+  // )
 
 
   return (
     <SafeAreaView>
       <MapView mapType="hybrid" style={styles.map}
-          initialRegion={initialRegion}
-          showsUserLocation={true}
-          showsMyLocationButton={true}
-          showsCompass={true}
-          onLongPress={(e) => { handleLongPress(e) }}
-        >
-  
-        {currentPosition && currentPos}
+        initialRegion={initialRegion}
+        showsUserLocation={true}
+        showsMyLocationButton={true}
+        showsCompass={true}
+        onLongPress={(infos) => handleLongPress(infos)}
+      >
+
+        {currentPos}
         {safePlacesMarkers}
         {alertsMarkers}
         {buddiesMarkers}
