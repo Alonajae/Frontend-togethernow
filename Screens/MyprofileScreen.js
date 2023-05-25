9323ebcbb07b56070662b11c46341f9414115349
camillepic <camillejpicard@gmail.com>
2023-05-25 12:39:07 +0200

    profile

--- Screens/MyprofileScreen.js
+++ Screens/MyprofileScreen.js
@@ -141,9 +141,11 @@ export default function MyProfileScreen({ navigation }) {
     <PaperProvider>
       <Portal>
         <View style={styles.container}>
+          <View style={styles.backContainer}>
           <Button style={styles.backBtn} onPress={handleBackToMap}>
             <Text style={styles.textBackBtn}>Back</Text>
           </Button>
+          </View>
           <Text style={styles.title}>My Profile</Text>
           <View style={styles.headerInfo}>
             <View style={styles.profileImageContainer}>
@@ -297,7 +299,6 @@ const styles = StyleSheet.create({
   },
 
   backBtn: {
-    flex : flex-start,
     width: 100,
     height: 40,
     alignItems: "center",
